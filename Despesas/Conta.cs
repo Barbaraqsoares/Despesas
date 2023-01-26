de625b955499eefa9207387a827ad98d711603e4
Barbara Queiroz <barbaraqs@gmail.com>
2023-01-25 23:43:56 -0300

    feat/iniciando o projeto de despesas em c#

---
+++ Despesas/Conta.cs
@@ -0,0 +1,11 @@
+namespace Despesas
+{
+    public class Conta
+    {
+        public int NumConta { get; set; }
+
+        public string NameConta { get; set; }
+
+        public double Saldo { get; set; }
+    }
+}
